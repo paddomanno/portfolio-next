e073824d51b359d13d61d6b3d8c065c6b16b1b50
Fender, Patrick <patricktfender+git@gmail.com>
2023-04-25 09:43:25 +0200

    first design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        100: "100vh",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
