049368b15ab6ff8a2a0bbc8c34e9d43aa5e11589
Fender, Patrick <patricktfender+git@gmail.com>
2023-04-26 18:34:19 +0200

    full design

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import { type AppType } from "next/dist/shared/lib/utils";
 
 import "~/styles/globals.css";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,32 @@ export default {
       height: {
         100: "100vh",
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(var(--tw-gradient-stops))",
+      },
+      animation: {
+        "spin-slow-reverse": "spin 20s linear infinite reverse",
+        "move-and-spin": "moveAndSpin 20s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          from: {
+            transform: "translateX(-200px) rotate(0deg)",
+          },
+          to: {
+            transform: "translateX(-200px) rotate(360deg)",
+          },
+        },
+        moveAndSpin: {
+          from: {
+            transform: "translateX(200px) rotate(0deg)",
+          },
+          to: {
+            transform: "translateX(200px) rotate(360deg)",
+          },
+        },
+      },
     },
   },
   plugins: [],
